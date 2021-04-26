97ad51e7f783f4986cb37c7837501159c85191d1
MohamedB <m.m.bassiouni@gmail.com>
2021-04-25 23:21:36 -0700

    updated js to change timeblock color according to past, present or future hour

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 var timeDisplayEl = $('#currentDay');
-var currentHour = moment().format('hh')
-console.log(currentHour);
-var hourLableEl = document.querySelectorAll(".hour-lable")
-console.log(hourLableEl.value);
+var currentHour = moment().format('H');
+var timeBlockArr = $(".timeblock");
+
+var textEl = document.querySelectorAll(".form-control");
+
+var saveBtn = document.querySelectorAll(".btn")
+var textInput = localStorage.getItem("textInput");
+function storeState() {
+  localStorage.setItem("textInput", textInput);
+}
+
+for (var i = 0; i < timeBlockArr.length; i++) {
+  var blockHour = i + 9
+  
+  if (blockHour > currentHour){
+    $(textEl[i]).addClass("bg-secondary");
+  } else if (blockHour == currentHour) {
+    $(textEl[i]).addClass("bg-danger");
+  } else {
+    $(textEl[i]).addClass("bg-success");
+  };
+};
+
+
 function displayTime() {
     var rightNow = moment().format('dddd MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
