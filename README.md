f5bbe0a7919cbacb83f4b8bd31f4149e7495668d
MohamedB <m.m.bassiouni@gmail.com>
2021-04-27 01:49:53 -0700

    updated functions for localstorage and add readme content

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # work-day-scheduler
+
+## html :
+### link all webapi to be used:
+link bootstrap.
+link fontawesome.
+link google fonts family.
+link stylesheet.
+link jquery and moment.js
+
+### construct timeblocks:
+one container for each timeblock.
+span for timeblock hour lable.
+text area to enter schedule.
+button to save input.
+repeat blocks for 9-5 timeblocks.
+
+## js file:
+declare variables in global scope.
+function to display current time.
+loope around timeblocks to set background color according to past, present and future status.
+function to grab saved input from local storage.
+function to save input to local storage.
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// declare variables in global scope
 var timeDisplayEl = $('#currentDay');
 var currentHour = moment().format('H');
 var timeBlockArr = $(".timeblock");
@@ -5,16 +6,21 @@ var timeBlockArr = $(".timeblock");
 var textEl = document.querySelectorAll(".form-control");
 
 var saveBtn = document.querySelectorAll(".btn")
-var textInput = localStorage.getItem("textInput");
-function storeState() {
-  localStorage.setItem("textInput", textInput);
-}
 
+// function to display current time. 
+function displayTime() {
+  var rightNow = moment().format('dddd MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+};
+
+setInterval(displayTime, 1000);
+
+// loope around timeblocks to set background color according to past, present and future status. 
 for (var i = 0; i < timeBlockArr.length; i++) {
   var blockHour = i + 9
   
   if (blockHour > currentHour){
-    $(textEl[i]).addClass("bg-secondary");
+    $(textEl[i]).addClass("bg-warning");
   } else if (blockHour == currentHour) {
     $(textEl[i]).addClass("bg-danger");
   } else {
@@ -22,10 +28,37 @@ for (var i = 0; i < timeBlockArr.length; i++) {
   };
 };
 
+// function to grab saved input from local storage 
+function init() {
 
-function displayTime() {
-    var rightNow = moment().format('dddd MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  };
-  setInterval(displayTime, 1000);
+    
+
+  timeBlockArr.each(function () {
+      var savedValue = $(this).attr("id");
+      var savedText = localStorage.getItem(savedValue);
+
+      if  (savedText !== null) {
+          $(this).children("savedText").val (savedText);
+          savedText.text() = savedText;
+      };
+  });
+};
+
+  
+//function to save input to local storage 
+for (var i; i < saveBtn.length; i++) {
+  saveBtn[i].on("click", function (event) {
+    event.preventDefault();
+    
+    var textInput = $(this).siblings(".form-control").val();
+
+    localStorage.setItem(textInput);
+    textInput.text() = textInput
+  } );
+};
+  
+
+
+
+init();
 
